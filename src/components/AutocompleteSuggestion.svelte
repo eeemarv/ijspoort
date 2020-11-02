c0916a8c7fc13d9b58debc7b534ffb3b4b06bc1f
martti <info@martti.be>
2020-11-02 12:56:41 +0100

    remove webpack / parse scss

---
+++ src/components/AutocompleteSuggestion.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let person;
+</script>
+
+<div class="d-flex w-100 justify-content-between">
+    <div>
+        {person.firstnames} {person.surname}
+    </div>
+    <div>
+        { person.phone_mobile }
+    </div>
+</div>
+<div class="d-flex w-100 justify-content-between">
+    <div>
+        {person.address_street_and_number}, {person.address_zip} {person.address_municipality}
+    </div>
+    <div>
+        {person.email}
+    </div>
+</div>
