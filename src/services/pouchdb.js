92ee40d3ca68f408312f08f3af1b4bce8bc3a45d
martti <info@martti.be>
2020-12-17 00:37:17 +0100

    auth conn

--- src/services/pouchdb.js
+++ src/services/pouchdb.js
@@ -5,8 +5,8 @@ PouchDB.plugin(require('pouchdb-authentication'));
 if (!env.DB_PREFIX){
     throw 'env DB_PREFIX not set';
 }
-if (!env.DB_URL){
-    throw 'env DB_URL not set';
+if (!env.DB_HOST){
+    throw 'env DB_HOST not set';
 }
 if (!env.DB_USERNAME){
     throw 'env DB_USERNAME not set';
@@ -15,7 +15,7 @@ if (!env.DB_PASSWORD){
     throw 'env DB_PASSWORD not set';
 }
 
-const db_url = env.DB_URL;
+const db_host = env.DB_HOST;
 const db_password = env.DB_PASSWORD;
 const db_username = env.DB_USERNAME;
 const db_remote_sync_enabled = env.DB_REMOTE_SYNC_ENABLED === '1' ? true : false;
@@ -39,98 +39,89 @@ const db_nfc = new PouchDB(i_nfc);
 const db_eid = new PouchDB(i_eid);
 const db_person = new PouchDB(i_person);
 
+const auth_conn = db_password + ':' + db_username + '@' + db_host;
+
 if (db_remote_sync_enabled){
-    const db_remote_reg = new PouchDB(db_url + i_reg, {skip_setup: true});
-    db_remote_reg.login(db_username, db_password).then(() => {
-        db_reg.sync(db_remote_reg, sync_options)
-        .on('change', function (info) {
-            console.log('CHANGE');
-            console.log(info);
-            // handle change
-        }).on('paused', function (err) {
-            console.log('PAUSED');
-            console.log(err);
-            // replication paused (e.g. replication up to date, user went offline)
-        }).on('active', function () {
-            console.log('ACTIVE');
-            // replicate resumed (e.g. new changes replicating, user went back online)
-        }).on('denied', function (err) {
-            console.log('DENIED');
-            console.log(err);
-            // a document failed to replicate (e.g. due to permissions)
-        }).on('complete', function (info) {
-            console.log('COMPLETE');
-            console.log(info);
-            // handle complete
-        }).on('error', function (err) {
-            console.log('UNHANDLED ERROR');
-            console.log(err);
-            // handle error
-        });
-    }).catch((err) => {
+    const db_remote_reg = new PouchDB(auth_conn + i_reg);
+    db_reg.sync(db_remote_reg, sync_options)
+    .on('change', function (info) {
+        console.log('CHANGE');
+        console.log(info);
+        // handle change
+    }).on('paused', function (err) {
+        console.log('PAUSED');
         console.log(err);
+            // replication paused (e.g. replication up to date, user went offline)
+    }).on('active', function () {
+        console.log('ACTIVE');
+            // replicate resumed (e.g. new changes replicating, user went back online)
+    }).on('denied', function (err) {
+        console.log('DENIED');
+        console.log(err);
+            // a document failed to replicate (e.g. due to permissions)
+    }).on('complete', function (info) {
+        console.log('COMPLETE');
+        console.log(info);
+            // handle complete
+    }).on('error', function (err) {
+        console.log('UNHANDLED ERROR');
+        console.log(err);
+        // handle error
     });
 
-    const db_remote_nfc = new PouchDB(db_url + i_nfc, {skip_setup: true});
-    db_remote_nfc.login(db_username, db_password).then(() => {
-        db_nfc.sync(db_remote_nfc, sync_options)
-        .on('change', function (info) {
-            console.log('CHANGE');
-            console.log(info);
-            // handle change
-        }).on('paused', function (err) {
-            console.log('PAUSED');
-            console.log(err);
-            // replication paused (e.g. replication up to date, user went offline)
-        }).on('active', function () {
-            console.log('ACTIVE');
-            // replicate resumed (e.g. new changes replicating, user went back online)
-        }).on('denied', function (err) {
-            console.log('DENIED');
-            console.log(err);
-            // a document failed to replicate (e.g. due to permissions)
-        }).on('complete', function (info) {
-            console.log('COMPLETE');
-            console.log(info);
-            // handle complete
-        }).on('error', function (err) {
-            console.log('UNHANDLED ERROR');
-            console.log(err);
-            // handle error
-        });
-    }).catch((err) => {
+    const db_remote_nfc = new PouchDB(auth_conn + i_nfc);
+    db_nfc.sync(db_remote_nfc, sync_options)
+    .on('change', function (info) {
+        console.log('CHANGE');
+        console.log(info);
+        // handle change
+    }).on('paused', function (err) {
+        console.log('PAUSED');
         console.log(err);
+        // replication paused (e.g. replication up to date, user went offline)
+    }).on('active', function () {
+        console.log('ACTIVE');
+        // replicate resumed (e.g. new changes replicating, user went back online)
+    }).on('denied', function (err) {
+        console.log('DENIED');
+        console.log(err);
+        // a document failed to replicate (e.g. due to permissions)
+    }).on('complete', function (info) {
+        console.log('COMPLETE');
+        console.log(info);
+        // handle complete
+    }).on('error', function (err) {
+        console.log('UNHANDLED ERROR');
+        console.log(err);
+        // handle error
     });
 
-    const db_remote_person = new PouchDB(db_url + i_person, {skip_setup: true});
-    db_remote_person.login(db_username, db_password).then(() => {
-        db_person.sync(db_remote_person, sync_options)
-        .on('change', function (info) {
-            console.log('CHANGE');
-            console.log(info);
-            // handle change
-        }).on('paused', function (err) {
-            console.log('PAUSED');
-            console.log(err);
-            // replication paused (e.g. replication up to date, user went offline)
-        }).on('active', function () {
-            console.log('ACTIVE');
-            // replicate resumed (e.g. new changes replicating, user went back online)
-        }).on('denied', function (err) {
-            console.log('DENIED');
-            console.log(err);
-            // a document failed to replicate (e.g. due to permissions)
-        }).on('complete', function (info) {
-            console.log('COMPLETE');
-            console.log(info);
-            // handle complete
-        }).on('error', function (err) {
-            console.log('UNHANDLED ERROR');
-            console.log(err);
-            // handle error
-        });
-    }).catch((err) => {
+    const db_remote_person = new PouchDB(auth_conn + i_person);
+
+    db_person.sync(db_remote_person, sync_options)
+    .on('change', function (info) {
+        console.log('CHANGE');
+        console.log(info);
+        // handle change
+    }).on('paused', function (err) {
+        console.log('PAUSED');
         console.log(err);
+        // replication paused (e.g. replication up to date, user went offline)
+    }).on('active', function () {
+        console.log('ACTIVE');
+        // replicate resumed (e.g. new changes replicating, user went back online)
+    }).on('denied', function (err) {
+        console.log('DENIED');
+        console.log(err);
+        // a document failed to replicate (e.g. due to permissions)
+    }).on('complete', function (info) {
+        console.log('COMPLETE');
+        console.log(info);
+        // handle complete
+    }).on('error', function (err) {
+        console.log('UNHANDLED ERROR');
+        console.log(err);
+        // handle error
     });
 }
 
