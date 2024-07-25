33b64df8080cc28d48edaaa47d70ceb602ef6c25
martti <info@martti.be>
2024-07-25 19:14:13 +0200

    filter un/valid regs

--- src/render_desk/Reg/RegList.svelte
+++ src/render_desk/Reg/RegList.svelte
@@ -5,6 +5,10 @@
   import RegAlertDesk from './RegAlertDesk.svelte';
   import ListGroup from '../../render/Common/ListGroup.svelte';
   import ListGroupItem from '../../render/Common/ListGroupItem.svelte';
+  import Checkbox from '../../render/Common/Checkbox.svelte';
+
+  let show_valid = true;
+  let show_unvalid = true;
 
   $: valid_index_map = new Map([...$reg_map.values()].filter((r) => typeof r.invalid === 'undefined').map((r, i) => [r._id, i + 1]));
 </script>
@@ -12,14 +16,41 @@
 <Row>
   <Col class=h-100>
     <ListGroup>
-      <li class="list-group-item bg-primary">
-          <h3>Registraties</h3>
+      <li class="list-group-item bg-darkgrey">
+        <Row class=mb-2>
+          <Col sm=6>
+            <h3>
+              Registraties <span class=sub>laatste 5 uur</span>
+            </h3>
+          </Col>
+          <Col>
+            <Checkbox name=show_valid
+              title="Toon geldige registraties"
+              bind:checked={show_valid}
+            >
+              Geldig: {valid_index_map.size}
+            </Checkbox>
+          </Col>
+          <Col>
+            <Checkbox name=show_unvalid
+              title="Toon ongeldige registraties"
+              bind:checked={show_unvalid}
+            >
+              Ongeldig: {$reg_map.size - valid_index_map.size}
+            </Checkbox>
+          </Col>
+        </Row>
       </li>
 
       <RegAlertDesk />
 
       {#each [...$reg_map].reverse() as [reg_id, reg](reg._id)}
+        {#if (show_valid && show_unvalid)
+          || (valid_index_map.has(reg_id) && show_valid)
+          || (!valid_index_map.has(reg_id) && show_unvalid)
+        }
         <RegItem {reg} count={valid_index_map.get(reg_id)} />
+        {/if}
       {/each}
 
       {#if $reg_map.size === 0}
@@ -34,5 +65,8 @@
 <style>
   li {
     border-bottom:  1px solid lightgrey;
+  }
+  span.sub {
+    font-size: .7em;
   }
 </style>
