d812e02fc265cea861a4abc239a587cee68c7393
martti <info@martti.be>
2025-05-12 15:03:31 +0200

    solve invalid seek

--- src/main/mfrc_2/spi.js
+++ src/main/mfrc_2/spi.js
@@ -10,8 +10,8 @@ class SPIDevice {
 
   transferSync(txBuffer) {
     const rxBuffer = Buffer.alloc(txBuffer.length);
-    fs.writeSync(this.fd, txBuffer, 0, txBuffer.length, 0);
-    fs.readSync(this.fd, rxBuffer, 0, rxBuffer.length, 0);
+    fs.writeSync(this.fd, txBuffer);
+    fs.readSync(this.fd, rxBuffer);
     return rxBuffer;
   }
 
